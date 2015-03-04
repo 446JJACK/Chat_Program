11dc5da3daf0d4838fd91e6e9a24d72a61784134
Kevin Boyette <Kevinxboyette@gmail.com>
2015-03-04 10:31:58 -0500

    worked on javascript

--- chatroom.js
+++ chatroom.js
@@ -1,24 +1,43 @@
 console.log('hello world');
 
-// not working completely.  It is a work in progress.  
-// try to get the value to appear. Put it in Div. 
+// not working completely.  It is a work in progress.
+// try to get the value to appear. Put it in Div.
 //
-// for some reason textbox is showing up as null. 
+// for some reason textbox is showing up as null.
 // I'm probably overlooking something small as it is 12:34 AM
 
 
-function clickSubmit(wrapper) {
-    wrapper.innerHTML = "<br>CLICK IS WORKING<br>";
+function clickSubmit() {
+
+    var textbox = document.getElementById("textbox").value.indexOf("@");
+    var divBox = document.getElementById("chatbox").value;
+    var submitButton = document.getElementById("submit").value;
+
+    if (submitButton = "send") {
+        console.log('true');
+
+    }
+
+    /*var wrapper = document.querySelector("chatbox");
+    //wrapper.innerHTML = "<br>CLICK IS WORKING<br>";
+    var newElement = document.createElement('div');
+    newElement.innerHTML = "Whats up?!";
     if (document.querySelector('textbox') != null) {
         alert('VALUE IS NOT NULL');
         var message = document.querySelector('textbox').value;
         wrapper.innerHTML = "<br>" + message + "<br>";
+        //document.querySelector('wrapper').appendChild(message);
 
     } else {
         var message = null;
         alert('VALUE IS NULL');
         console.log(message);
     }
+    */
 }
 
+
+window.addEventListener("load", function() {
+    clickSubmit();
+});
 
