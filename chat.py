6171bf7f564a9310593c168d43b7bd66cf4b3795
James Livulpi <jlivu001@plattsburgh.edu>
2015-03-10 19:34:00 -0400

    Fixed weird formatting issue

--- chat.py
+++ chat.py
@@ -53,14 +53,8 @@ class ChatFactory(Factory):
 
 def main():
     ##log.startLogging(sys.stdout) ##Uncomment for debugging purposes
-<<<<<<< HEAD
     reactor.listenTCP(8062, ChatFactory())
-=======
-    reactor.listenTCP(8080, ChatFactory())
->>>>>>> c3376ba3a5a00c442e4164196f2830c4cc8c3099
     reactor.run()
-    
-
 
 if __name__ == "__main__":
     main()
