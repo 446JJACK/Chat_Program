b272adf490516b672bd40fb9d5ff16469a0bd71f
James Livulpi <jlivu001@cslab-109.cs.plattsburgh.edu>
2015-03-11 10:50:47 -0400

    sqlite3 database

---
+++ db.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+#TO RUN GO TO COMMAND LINE AND TYPE -> sqlite3 users.db AND THEN
+# select * from users;
+##WILL SHOW WHATS IN THE DATABASE
+
+
+import sqlite3
+
+#creat a database in RAM
+db = sqlite3.connect('users.db')
+
+
+cursor = db.cursor()
+cursor.execute('''
+    CREATE TABLE users(ID INTEGER PRIMARY KEY, accountname TEXT, password TEXT,
+                       email TEXT, displayname TEXT)
+              ''')
+#EXAMPLE DATA##
+accountname = "jlivu001"
+password = "12345"
+email = "james@gmail.com"
+displayname = "james"
+
+#Insert user 1 example
+cursor.execute('''INSERT INTO users(accountname, password, email, displayname)
+                  VALUES(?,?,?,?)''',(accountname, password, email, displayname))
+
+print('First user inserted')
+db.commit()
