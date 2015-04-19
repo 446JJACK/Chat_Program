760e43dd87617ab7dca341f4d0449636b8511484
James Livulpi <jlivu001@plattsburgh.edu>
2015-04-19 16:14:41 -0400

    nothing special

--- python/presentationCodeDatabase.py
+++ python/presentationCodeDatabase.py
@@ -2,7 +2,7 @@
 
 import sqlite3
 
-DATABASE = 'test.db'
+DATABASE = 'test.db' #GLOBAL
 
 
 def main():
@@ -15,19 +15,25 @@ def main():
 
     
     #cursor = conn.cursor()
-    #cursor.execute('''CREATE TABLE groupMembers(ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    #                firstName TEXT,lastName, email TEXT, phoneNumber INT)''')
+    #cursor.execute('''CREATE TABLE groupMembers
+    #           (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    #           firstName VARCHAR(30),
+    #           lastName VARCHAR(30),
+    #           email VARCHAR(50),
+    #           phoneNumber INT)''')
 
-    firstName = input("Please Enter the first name of your group member: ")
-    lastName = input("Please Enter the last name of your group member: ")
-    email = input("Please enter the email of your group member: ")
-    phoneNumber = input("Please enter the phone number of your group member: ")
+    firstName = input("Please Enter the first name of your group member: ",)
+    lastName = input("Please Enter the last name of your group member: ",)
+    email = input("Please enter the email of your group member: ",)
+    phoneNumber = input("Please enter the phone number of your group member: ",)
 
+    
     cursor.execute('''INSERT INTO groupMembers(firstName,lastName, email, phoneNumber)
-                        VALUES(?,?,?,?)''',(firstName, lastName, email, phoneNumber))
+                        VALUES(?,?,?,?)''',(firstName, lastName, email, phoneNumber,))
     print("Group member added! ")
 
-    db.commit()
+
+    db.commit() #Commits the change to database
     db.close()
 main()
     
