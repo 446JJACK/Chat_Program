c1a3d588af92a25cd0eef975323d96da06b5d035
AshleyRegan <AshleyRegan@users.noreply.github.com>
2015-05-04 10:03:30 -0400

    I think this is about it

--- js/chat.js
+++ js/chat.js
@@ -1,38 +1,37 @@
-function onlineUsrUpdate() {
-  var curTab = document.querySelector("[type=radio]:checked ~ label ~ .content");
-  var onlineUL = document.querySelector("ul#online");
-  //Get users in current chatroom
-}
-
 function countUsrs() {
   var numUsrs = document.querySelector("p#numUsrs");
   var onlineUsrs = document.querySelector("ul#online");
   var i = 0;
   var num = 0;
   while (document.getElementsByClassName('usr')[i++]) num++;
-  numUsrs.innerHTML = num
+  numUsrs.innerHTML = num;
 }
 
-function countTabs() {
-  var i = 0;
-  var divCount = 0;
-  while (document.getElementsByClassName('tab')[i++]) divCount++;
-  return (divCount);
+function fontStuff(mp) {
+  var cur = document.querySelector("textarea#msg");
+  var siz = document.defaultView.getComputedStyle(cur, null).getPropertyValue('font-size');
+  var col = document.defaultView.getComputedStyle(cur, null).getPropertyValue('color');
+  var sty = document.defaultView.getComputedStyle(cur, null).getPropertyValue('font-family');
+  mp.style.setProperty('font-size', siz);
+  mp.style.setProperty('color', col);
+  mp.style.setProperty('font-family', sty);
 }
 
 function sendMsg() {
   var curChatBox = document.querySelector("[type=radio]:checked ~ label ~ .content");
   var msg = document.createElement('p');
-  msg.innerHTML = document.querySelector("input#msg").value;
-
-  document.querySelector("input#msg").value = "";
-
+  fontStuff(msg);
+  msg.innerHTML = document.querySelector("textarea#msg").value;
+  document.querySelector("textarea#msg").value = "";
   curChatBox.appendChild(msg);
+  curChatBox.scrollTop = curChatBox.scrollHeight;
 }
 
 function addChatRm() {
   var tabContainer = document.querySelector(".tabs");
-  var numTabs = countTabs();
+  var numTabs = tabContainer.childElementCount;
+  var tt = tabContainer.lastChild.previousSibling;
+  var nt = tt.previousSibling.previousSibling;
   var newTab = document.createElement("div");
   newTab.className = "tab";
 
@@ -44,9 +43,6 @@ function addChatRm() {
 
   var newLabel = document.createElement('label');
   newLabel.setAttribute('for', "tab" + numTabs);
-  //var tempLabel = document.createElement('input');
-  //newLabel.innerHTML = tempLabel;
-  //tempLabel.addEventListener() //OI! LOOK AT EVENT TYPES!!
   newLabel.innerHTML = "Chat " + numTabs;
 
   var newContent = document.createElement('div');
@@ -55,19 +51,32 @@ function addChatRm() {
   newTab.appendChild(newRadio);
   newTab.appendChild(newLabel);
   newTab.appendChild(newContent);
-
-  tabContainer.insertBefore(newTab, tabContainer.childNodes[numTabs + 1]);
+  tabContainer.insertBefore(newTab, tt);
 }
 
 function init() {
   var sendbtn = document.querySelector("button#sendbtn");
   var addTab = document.getElementById("addTab");
+  var msgbox = document.querySelector("textarea#msg");
+
+  console.log(startWidth = document.defaultView.getComputedStyle(msgbox).width); //parseInt(document.defaultView.getComputedStyle(msgbox).width, 10));
+  sendbtn.style.left = (msgbox.clientWidth + 20) + "px";
+
+  msgbox.addEventListener("mouseover", function() {
+    msgbox.style.resize = "both";
+    msgbox.style.minWidth = "10%";
+    window.addEventListener("mousemove", function() {
+      sendbtn.style.left = (msgbox.clientWidth + 20) + "px";
+    });
+  });
+
   sendbtn.addEventListener("click", function() {
     sendMsg();
   });
 
   document.onkeydown = function(e) {
-    if ((e || window.event).keyCode == '13') {
+    if ((e || window.event).keyCode == '13' && !(e || window.event).shiftKey) {
+      (e || window.event).preventDefault();
       sendMsg();
     }
   };
@@ -79,93 +88,79 @@ function init() {
 
 window.addEventListener("load", function() {
   init();
-  //onlineUsrUpdate();
   countUsrs();
 });
 
-
-
-/*                Popup Javascript!         */
-
-
-function overlay()
-{
-    var el = document.querySelector("#overlay");
-    el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+/* Popup Javascript! */
+function overlay() {
+  var el = document.querySelector("#overlay");
+  el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
 }
 
-
-function overlaySignIn()
-{
-    var sin = document.querySelector("#overlaySignIn");
-    var div = document.querySelector("#grey");
-    sin.style.visibility = (sin.style.visibility == "visible") ? "hidden" : "visible";
-    //div.style.visibility = (div.style.visibility == "visible") ? "hidden" : "visible";
-//    div.style.opacity = (div.style.opacity == "1") ? "0" : "1";
+function overlaySignIn() {
+  var sin = document.querySelector("#overlaySignIn");
+  var div = document.querySelector("#grey");
+  sin.style.visibility = (sin.style.visibility == "visible") ? "hidden" : "visible";
+  //div.style.visibility = (div.style.visibility == "visible") ? "hidden" : "visible";
+  // div.style.opacity = (div.style.opacity == "1") ? "0" : "1";
 }
 
-function overlayRegister()
-{
-    var sin = document.querySelector("#overlayRegister");
-    var div = document.querySelector("#grey");
-    sin.style.visibility = (sin.style.visibility == "visible") ? "hidden" : "visible";
-    //div.style.visibility = (div.style.visibility == "visible") ? "hidden" : "visible";
-//    div.style.opacity = (div.style.opacity == "1") ? "0" : "1";
+function overlayRegister() {
+  var sin = document.querySelector("#overlayRegister");
+  var div = document.querySelector("#grey");
+  sin.style.visibility = (sin.style.visibility == "visible") ? "hidden" : "visible";
+  //div.style.visibility = (div.style.visibility == "visible") ? "hidden" : "visible";
+  // div.style.opacity = (div.style.opacity == "1") ? "0" : "1";
 }
 
+function overlaySettings() {
+  var sin = document.querySelector("#overlaySettings");
+  var div = document.querySelector("#grey");
+  var cur = document.querySelector("textarea#msg");
+  sin.style.visibility = (sin.style.visibility == "visible") ? "hidden" : "visible";
+  var e = document.querySelector("select#color");
+  var f = document.querySelector("select#size");
+  var g = document.querySelector("select#font");
+  var colorset = e.options[e.selectedIndex].value;
+  var sizeset = f.options[f.selectedIndex].value;
+  var fontset = g.options[g.selectedIndex].value;
+  cur.style.setProperty('font-size', sizeset);
+  cur.style.setProperty('color', colorset);
+  cur.style.setProperty('font-family', fontset);
+}
 
-function login()
-{
+function login() {
   /*
-  var userName = document.querySelector("#sinUserName").value;
-  var password = document.querySelector("#sinPassword").value;
-
-  console.log(userName + " " + password);
-
-  document.querySelector("#sinUsername").value = "";
-  document.querySelector("#sinPassword").value = "";
+var userName = document.querySelector("#sinUserName").value;
+var password = document.querySelector("#sinPassword").value;
+console.log(userName + " " + password);
+document.querySelector("#sinUsername").value = "";
+document.querySelector("#sinPassword").value = "";
 */
   overlaySignIn();
 }
 
-
-function register()
-{
+function register() {
   /*var userName = document.querySelector("#regUsername").value;
-  var email = document.querySelector("#regEmail").value;
-  var password = document.querySelector("#regPassword").value;
-  var confPassword = document.querySelector("#regConfPassword").value;
-
-
-  console.log(userName + email + password + confPassword);
-
-  document.querySelector("#regUsername").value = "";
-  document.querySelector("#regEmail").value = "";
-  document.querySelector("#regPassword").value = "" ;
-  document.querySelector("#regConfPassword").value = "";
+var email = document.querySelector("#regEmail").value;
+var password = document.querySelector("#regPassword").value;
+var confPassword = document.querySelector("#regConfPassword").value;
+console.log(userName + email + password + confPassword);
+document.querySelector("#regUsername").value = "";
+document.querySelector("#regEmail").value = "";
+document.querySelector("#regPassword").value = "" ;
+document.querySelector("#regConfPassword").value = "";
 */
   overlayRegister();
-
 }
 
-
-function swapRegisterAndSignIn()
-{
-    var sin = document.querySelector("#overlaySignIn");
-    var div = document.querySelector("#grey");
-    var reg = document.querySelector("#overlayRegister");
-
-    sin.style.visibility = (sin.style.visibility == "visible") ? "hidden" : "visible";
-    //div.style.visibility = (div.style.visibility == "visible") ? "hidden" : "visible";
-//    div.style.opacity = (div.style.opacity == "1") ? "0" : "1";
-
-    reg.style.visibility = (reg.style.visibility == "visible") ? "hidden" : "visible";
-
-
+function swapRegisterAndSignIn() {
+  var sin = document.querySelector("#overlaySignIn");
+  var div = document.querySelector("#grey");
+  var reg = document.querySelector("#overlayRegister");
+  sin.style.visibility = (sin.style.visibility == "visible") ? "hidden" : "visible";
+  //div.style.visibility = (div.style.visibility == "visible") ? "hidden" : "visible";
+  // div.style.opacity = (div.style.opacity == "1") ? "0" : "1";
+  reg.style.visibility = (reg.style.visibility == "visible") ? "hidden" : "visible";
 }
-
-
-/*              END POP UP JAVASCRIPT  */
-
-
-
+/* END POP UP JAVASCRIPT */
